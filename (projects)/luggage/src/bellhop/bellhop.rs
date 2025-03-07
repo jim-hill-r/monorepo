d203f538a8b25e427ec488933de310194d42af9a
Jim Hill <jim.hill.r@gmail.com>
2025-03-07 07:32:42 -0800

    test: add bellhop end to end test

--- (projects)/luggage/src/bellhop/bellhop.rs
+++ (projects)/luggage/src/bellhop/bellhop.rs
@@ -13,7 +13,7 @@ use uuid::Uuid;
 
 use crate::{
     closet::{
-        closet::{Closet, ClosetBuiltinType, ClosetCreator, ClosetReader},
+        closet::{Closet, ClosetBuiltinType, ClosetCreator},
         providers::surrealdb::SurrealDbClosetProvider,
     },
     core::core::LuggageId,
@@ -118,14 +118,30 @@ async fn create_cube(
 mod tests {
     use crate::{
         closet::closet::Closet,
-        cube::cube::{CubeDefinition, CubeRegistration},
+        cube::cube::{CubeDefinition, CubeRegistration, CubeSchema},
         error::Result,
     };
     use axum_test::TestServer;
+    use schemars::{schema_for, JsonSchema};
     use serde_json::json;
     use uuid::Uuid;
 
     use super::*;
+
+    #[derive(Serialize, JsonSchema)]
+    struct TestContent {
+        name: String,
+        todos: Vec<String>,
+    }
+
+    impl CubeRegistration for TestContent {
+        fn id() -> LuggageId {
+            return Uuid::try_parse("0194f2fe-6f7a-7dd2-8af3-d6d4c9a2f74a").unwrap_or_default();
+        }
+        fn schema() -> CubeSchema {
+            return serde_json::to_string_pretty(&schema_for!(TestContent)).unwrap_or_default();
+        }
+    }
 
     #[tokio::test]
     async fn create_cube_definition() -> Result<()> {
@@ -165,10 +181,47 @@ mod tests {
             .to_string(),
         };
         let response = server.post("/v1/cube").json(&cube).await;
-        println!("{}", response.status_code());
         response.assert_status(StatusCode::CREATED);
         Ok(())
     }
 
-    // TODO: Write tests similar to a client where some arbitrary cube def gets created, then pushed into various providers
+    #[tokio::test]
+    async fn create_test_content_in_default_provider() -> Result<()> {
+        let test_name = "create_test_content_using_default_provider";
+        let server = TestServer::new(app(None).await?).unwrap(); //TODO: Remove unwrap (use ?)
+        let test_cube_definition_id = Uuid::now_v7();
+        let test_cube_definition = CreateCube {
+            bellhop_header: BellhopHeader { closet_id: None },
+            cube_header: CubeHeader {
+                id: test_cube_definition_id,
+                definition: CubeDefinition::id(),
+            },
+            content: json!({
+                "id": TestContent::id(),
+                "schema": TestContent::schema()
+            })
+            .to_string(),
+        };
+        let definition_response = server.post("/v1/cube").json(&test_cube_definition).await;
+        definition_response.assert_status(StatusCode::CREATED);
+
+        let test_cube_id = Uuid::now_v7();
+        let test_content = TestContent {
+            name: test_name.into(),
+            todos: vec!["todo_one".into(), "todo_two".into()],
+        };
+        let test_cube = CreateCube {
+            bellhop_header: BellhopHeader { closet_id: None },
+            cube_header: CubeHeader {
+                id: test_cube_id,
+                definition: TestContent::id(),
+            },
+            content: serde_json::to_string(&test_content)
+                .expect("test_content should serialize to json."),
+        };
+        let test_response = server.post("/v1/cube").json(&test_cube_definition).await;
+        test_response.assert_status(StatusCode::CREATED);
+
+        Ok(())
+    }
 }
