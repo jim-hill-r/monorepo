a29e473a4420b16ef4b239e631cba4a7ae20524b
Jim Hill <jim.hill.r@gmail.com>
2025-05-29 07:44:13 -0700

    fix(cast_vscode): session time status was using wrong root folder

--- (projects)/cast_vscode/src/extension.ts
+++ (projects)/cast_vscode/src/extension.ts
@@ -3,44 +3,52 @@ import fs from "fs";
 
 let myStatusBarItem: vscode.StatusBarItem;
 
-export function activate({
-  extensionPath,
-  subscriptions,
-}: vscode.ExtensionContext) {
+const SESSIONS_DIRECTORY = ".cast/sessions";
+
+export function activate({ subscriptions }: vscode.ExtensionContext) {
   myStatusBarItem = vscode.window.createStatusBarItem(
     vscode.StatusBarAlignment.Left,
     100
   );
   subscriptions.push(myStatusBarItem);
 
-  updateStatusBarItem(`${extensionPath}/.cast/sessions`);
+  updateStatusBarItem();
 }
 
 export function deactivate() {}
 
-function updateStatusBarItem(sessionDirectory: string): void {
-  fs.readdir(sessionDirectory, (err, files) => {
-    if (!err && files.length > 0) {
-      const sessionLogFilePath = `${sessionDirectory}/${files[0]}`;
-      const sessionLogContent = fs.readFileSync(sessionLogFilePath, "utf-8");
-      const sessionStart = getSessionStart(sessionLogContent);
-      if (sessionStart) {
-        const elapsed = Math.floor(
-          (Date.now() - sessionStart.getTime()) / 1000
-        );
-        const hours = Math.floor(elapsed / 3600);
-        const minutes = Math.floor((elapsed % 3600) / 60);
-        const seconds = elapsed % 60;
-        myStatusBarItem.text = `${String(hours).padStart(2, "0")}:${String(
-          minutes
-        ).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+function updateStatusBarItem(): void {
+  if (
+    vscode.workspace.workspaceFolders !== undefined &&
+    vscode.workspace.workspaceFolders.length > 0
+  ) {
+    console.log(vscode.workspace.workspaceFolders);
+    const sessionDirectory = `${vscode.workspace.workspaceFolders[0].uri.path}/${SESSIONS_DIRECTORY}`;
+    fs.readdir(sessionDirectory, (err, files) => {
+      if (!err && files.length > 0) {
+        const sessionLogFilePath = `${sessionDirectory}/${files[0]}`;
+        const sessionLogContent = fs.readFileSync(sessionLogFilePath, "utf-8");
+        const sessionStart = getSessionStart(sessionLogContent);
+        if (sessionStart) {
+          const elapsed = Math.floor(
+            (Date.now() - sessionStart.getTime()) / 1000
+          );
+          const hours = Math.floor(elapsed / 3600);
+          const minutes = Math.floor((elapsed % 3600) / 60);
+          const seconds = elapsed % 60;
+          myStatusBarItem.text = `${String(hours).padStart(2, "0")}:${String(
+            minutes
+          ).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+        }
       }
-    }
-  });
+    });
 
-  myStatusBarItem.show();
-
-  setTimeout(() => updateStatusBarItem(sessionDirectory), 2000);
+    myStatusBarItem.show();
+  } else {
+    myStatusBarItem.text = `00:00:00`;
+    myStatusBarItem.hide();
+  }
+  setTimeout(() => updateStatusBarItem(), 1000);
 }
 
 function getSessionStart(sessionLog: string): Date | undefined {
