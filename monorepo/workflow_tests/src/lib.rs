117b2dd8f6e8a94b8e3a48d14f7a52187f9cc6b1
Jim Hill <25872250+jim-hill-r@users.noreply.github.com>
2026-03-13 21:10:30 -0700

    fix: nightly

--- monorepo/workflow_tests/src/lib.rs
+++ monorepo/workflow_tests/src/lib.rs
@@ -57,9 +57,9 @@ pub fn get_cast_cli_cargo_path() -> PathBuf {
     get_repo_root().join("cast/cli/Cargo.toml")
 }
 
-/// Get the path to the cast-cd.yml workflow file
+/// Get the path to the cd.yml workflow file
 pub fn get_cast_cd_workflow_path() -> PathBuf {
-    get_repo_root().join(".github/workflows/cast-cd.yml")
+    get_repo_root().join(".github/workflows/cd.yml")
 }
 
 /// Get the path to the codeql.yml workflow file
